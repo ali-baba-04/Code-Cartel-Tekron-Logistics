e809a8e592e6d6b1992945f3c6f4cbb4444f10f4
CodeX047 <vishalrp047@gmail.com>
2026-02-01 05:22:37 +0530

    feat: update routing and dashboard structure in App component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,26 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./landing/LandingPage";
 import Login from "./pages/Login";
-import SignUp from "./pages/SignUp";
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
+import Signup from "./pages/Signup";
+import DashboardRouter from "./pages/DashboardRouter";
+import OwnerDashboard from "./pages/OwnerDashboard";
+import UserDashboard from "./pages/UserDashboard";
+import DriverDashboard from "./pages/DriverDashboard";
+import TruckLogin from "./pages/TruckLogin";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-      </Routes>
-    </BrowserRouter>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/truck-login" element={<TruckLogin />} />
+        <Route path="/dashboard" element={<DashboardRouter />} />
+        <Route path="/dashboard/owner" element={<OwnerDashboard />} />
+        <Route path="/dashboard/user" element={<UserDashboard />} />
+        <Route path="/dashboard/driver" element={<DriverDashboard />} />
       </Routes>
     </BrowserRouter>
   );
